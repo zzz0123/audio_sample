052691311394a6f116ef9cce4932f4b9844bdada
zzz0123 <zzz0123@test.com>
2016-06-15 00:44:03 +0900

    Add stop button

--- audio_context.js
+++ audio_context.js
@@ -1,19 +1,30 @@
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 var context = new AudioContext();
 var reader = new FileReader;
+var source;
 
 reader.onload = function () {
   context.decodeAudioData(reader.result, function(buffer) { playSound(buffer) });
 }
 
 var playSound = function(buffer) {
-  var source = context.createBufferSource();
+  stopSound();
+  source = context.createBufferSource();
   source.buffer = buffer;
   source.connect(context.destination);
   source.start(0);
 };
 
+var stopSound = function() {
+  if (source) {
+    source.stop();
+  }
+}
+
 window.onload = function () {
-  var f = document.getElementById('upload')
+  var f = document.getElementById('upload');
   f.addEventListener('change', function(e) { reader.readAsArrayBuffer(e.target.files[0]) });
+
+  var s = document.getElementById('stop');
+  s.onclick = function() { stopSound() };
 }
